70cd6b0acf13a614b579f7f0b0c5bd0ba5a57ef7
Thomas Maxey <tmaxey201@gmail.com>
2021-06-24 13:26:40 -0400

    created inquier to add department and roles

--- server.js
+++ server.js
@@ -91,3 +91,68 @@ function viewEmployees(){
         });
 };
 
+function addDepartment(){
+    inquirer
+        .prompt({
+            name: "department",
+            type: "input",
+            message: "What is the name of the new department?",
+        })
+        .then(function(answer){
+            const query = "INSERT INTO department (name) VALUES ( ? )";
+            connection.query(query, answer.department, function(err, res){
+                console.log(`You have added: ${(answer.department).toUpperCase()}.`)
+            })
+            viewDepartments();
+        })
+}
+
+function addRole(){
+    connection.query("SELECT * FROM department", function(err, res){
+        if(err) throw (err);
+        inquirer
+        .prompt([{
+            name: "title",
+            type: "input",
+            message: "What is the title of the new role?",
+        },
+        {
+            name: "salary",
+            type: "input",
+            message: "What is the salary of the new role?",
+        },
+        {
+            name: "departmentName",
+            type: "list",
+            message: "Which department does this role fall into?",
+            choices: function() {
+                const choicesArray = [];
+                res.forEach(res => {
+                    choicesArray.push(
+                        res.name
+                    );
+                })
+                return choicesArray;
+            }
+        }
+    ])
+    .then(function(answer){
+        const department = answer.departmentName;
+        connection.query('SELECT * FROM DEPARTMENT', function(err, res){
+            if(err) throw(err);
+            let filteredDept = res.filter(function(res){
+                return res.name == department;
+            })
+            let id = filteredDept[0].id;
+            let query = 'INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)';
+            let values = [answer.title, parseInt(answer.salary), id]
+            console.log(values);
+            connection.query(query, values, function(err, res, fields){
+                console.log(`You have added the role: ${(values[0].toUpperCase())}.`)
+            })
+            viewRoles()
+        })
+    })
+    })
+}
+
